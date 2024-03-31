3bedb5164af8d741a97308eb2a3ae065d3b85334
ptrvsrg <s.petrov1@g.nsu.ru>
2024-03-31 21:07:08 +0700

    TEMPLATE-13: Добавлено к сущности время создания и последнего изменения

--- src/main/resources/db/changelog/DDL/000-create-template-table.sql
+++ src/main/resources/db/changelog/DDL/000-create-template-table.sql
@@ -4,12 +4,16 @@ CREATE TABLE IF NOT EXISTS templates
     code        VARCHAR(255) NOT NULL UNIQUE,
     name        VARCHAR(255) NOT NULL UNIQUE,
     description TEXT,
-    parameters  JSONB
+    parameters  JSONB,
+    created_at  TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ NOT NULL
 );
 
 COMMENT ON TABLE templates IS 'Шаблоны';
-COMMENT ON COLUMN templates.id IS 'ID шаблона';
-COMMENT ON COLUMN templates.code IS 'Код шаблона';
-COMMENT ON COLUMN templates.name IS 'Название шаблона';
-COMMENT ON COLUMN templates.description IS 'Описание шаблона';
-COMMENT ON COLUMN templates.parameters IS 'Параметры шаблона';
+COMMENT ON COLUMN templates.id IS 'ID';
+COMMENT ON COLUMN templates.code IS 'Код';
+COMMENT ON COLUMN templates.name IS 'Название';
+COMMENT ON COLUMN templates.description IS 'Описание';
+COMMENT ON COLUMN templates.parameters IS 'Параметры';
+COMMENT ON COLUMN templates.created_at IS 'Время создания';
+COMMENT ON COLUMN templates.updated_at IS 'Время последнего изменения';
